6d989f6e8aca0bb08c303e7d131f635ef1a944c6
Leandro Collares <leco@uvic.ca>
2019-05-31 18:31:25 -0300

    Refactor app component with an arrow function

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="app">
-      <h1>Foody</h1>
-    </div>
-  );
-}
+const App = () => (
+  <div className="app">
+    <h1>Foody</h1>
+  </div>
+);
 
 export default App;
