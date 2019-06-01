57fa54d3adb513a3290907f2f2f73c9674029481
Leandro Collares <leco@uvic.ca>
2019-06-01 13:36:20 -0300

    Add RestaurantListScreen component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import RestaurantListScreen from './components/RestaurantListScreen';
+import data from './data';
 import './App.css';
 
 const App = () => (
   <div className="app">
     <h1>Foody</h1>
+    <RestaurantListScreen restaurants={data} />
   </div>
 );
 
---
+++ src/components/RestaurantListScreen.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const RestaurantListScreen = ({ restaurants }) => (
+  <div className="restaurantList">
+    <p>Restaurant list should go here</p>
+  </div>
+);
+
+export default RestaurantListScreen;
